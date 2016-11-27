11473f697b2f8928fa452a73dd250a026ad024c9
Mahendra Ramesh <mahendra7@gmail.com>
2016-11-27 14:22:52 -0800

    Initializes the marketplace api rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MarketPlaceApi::Application.config.secret_key_base = '895628ebbf654721c2a36222635313961a6cc43ef07536fd81da910266f088db7e6401c257b2b50fbc4b81824840bb4aa814fc81acf95bbbef852f4d4588eb0c'
