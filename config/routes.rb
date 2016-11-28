ee300ecd4a01576c7bfa278682c273c0d90c49b7
Mahendra Ramesh <mahendra7@gmail.com>
2016-11-27 16:46:33 -0800

    Adds shoulda matchers 2.6.1 for spec refactors.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 require 'api_constraints'
 
-
 MarketPlaceApi::Application.routes.draw do
-  devise_for :users
   # Api definition
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
