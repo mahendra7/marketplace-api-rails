ed39d95aabe2a21d318b56851b3cc32b095741e9
Mahendra Ramesh <mahendra7@gmail.com>
2016-11-28 23:48:42 -0800

    User and Product association with tests.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,7 +18,22 @@ describe User do
   it { should allow_value('example@domain.com').for(:email) }
   # we test the auth_token is unique
   it { should validate_uniqueness_of(:auth_token)}
+  it { should have_many(:products) }
   
+  describe "#products association" do
+    before do
+      @user.save
+      3.times { FactoryGirl.create :product, user: @user }
+    end
+    it "destroys the associated products on self destruct" do
+      products = @user.products
+      @user.destroy
+      products.each do |product|
+        expect(Product.find(product)).to raise_error ActiveRecord::RecordNotFound
+      end
+    end
+  end
+    
   describe "#generate_authentication_token!" do
     it "generates a unique token" do
       Devise.stub(:friendly_token).and_return("auniquetoken123")
